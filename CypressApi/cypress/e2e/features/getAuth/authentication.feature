44fcc8add6499ae81390c14260d387cf545a9ebe
Yosyp Voloshchuk <yvoloshchuk@atlassian.com>
2024-06-03 23:06:32 +0300

    try

--- CypressApi/cypress/e2e/features/getAuth/authentication.feature
+++ CypressApi/cypress/e2e/features/getAuth/authentication.feature
@@ -2,11 +2,10 @@
 Feature: Authentication
 
   Scenario: Get user info
-    Given I am logged in user
     When GET USER info
     Then Status code is equal 200
     And Values 'user', 'username' in body are equal 'Yosyp Voloshchuk'
 
   Scenario: Create folder
     When Create Folder
-    Then Status code is equal 200
+    Then Status code is equal 201
--- CypressApi/cypress/e2e/steps_definition/folder_steps/folderSteps.ts
+++ CypressApi/cypress/e2e/steps_definition/folder_steps/folderSteps.ts
@@ -3,11 +3,11 @@ import {When} from "@badeball/cypress-cucumber-preprocessor";
 import {config} from '@config/config';
 
 
-// const BASE_URL = Cypress.env('base_url') as string;
+ const BASE_URL = Cypress.env('base_url') as string;
 const CREATE_FOLDER_URL = config.url.folder.create_folder
 
 When('Create Folder', () => {
     cy.fixture('folders/create_folder.json').then((body) =>{
-        cy.sentRequest('post', `${CREATE_FOLDER_URL}`, body);
+        cy.sentRequest('post', `${BASE_URL}/${CREATE_FOLDER_URL}`, body);
     })
 });
