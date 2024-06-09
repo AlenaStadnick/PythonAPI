4eb4ab9800626f2b35866bd2a630fc2247a11e5d
Yosyp Voloshchuk <yvoloshchuk@atlassian.com>
2024-06-09 17:30:57 +0300

    add python

--- API-Testing-Pytest-main/Tests/test_users.py
+++ API-Testing-Pytest-main/Tests/test_users.py
@@ -10,7 +10,8 @@ load_dotenv()
 base_url = os.getenv('BASE_URL')
 session = Session(base_url=base_url)
 
-def test_fetch_user() :
+def test_get_user() :
+
     path = '/user'
     response = session.request('GET', path)
     responseJson = json.loads(response.text)
@@ -19,3 +20,21 @@ def test_fetch_user() :
     id = response.json()['user']['id']
     assert (id != 'undefined' )
 
+
+def test_create_folder() :
+
+    path = '/space/90151393719/folder'
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    with open('TestData/create_folder.json', 'r') as file:
+        data = json.load(file)
+
+    # Make the POST request
+    response = session.request('post', path, headers=headers, data=json.dumps(data))
+
+    responseJson = json.loads(response.text)
+    print(responseJson)
+    assert response.status_code == 200
+    id = response.json()['id']
+    assert (id != 'undefined' )
